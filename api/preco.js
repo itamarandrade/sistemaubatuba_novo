4a37690c3a1b73751048b9724ecb5f69c2af28de
itamar <itamarbarbosa.13@gmail.com>
2022-08-03 10:13:08 -0300

    ordenação da tabela de preço

--- api/preco.js
+++ api/preco.js
@@ -13,14 +13,20 @@ $(document).ready(function () {
             success: function (result) {
                console.log(result);
                const R = Object.values(result.itens);
-               
+                R.sort((a, b) => (a.valor > b.valor)? 1 : (a.valor < b.valor) ? -1: 0);
+                
                 for (let i in R){
+                
                     
                     var newRow = $("<tr>");
                     var col = "";
+                    var linha = "";
                     col+= '<tr><td>'+ R[i].descricaoSite +'</td><td> R$ '+ parseFloat(R[i].valor, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +'</td></tr>';
-                    
+                    linha+='<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">'+ R[i].descricaoSite+'<p id="precomotof">R$ '+ parseFloat(R[i].valor, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,")+ '</p></span>';
+
                     $('.preco>tbody').append(col);
+
+                    $('#tabelaprecosf').append(linha);
 
                 }
             },
